1ea823805a7974a35c07c5b2ceffd61404428470
Sourav Kumar <skumar4230@gmail.com>
2026-09-13 17:13:17 +0530

    added timeout for downstream

--- src/main/java/com/example/kafkaorders/config/HttpClientConfig.java
+++ src/main/java/com/example/kafkaorders/config/HttpClientConfig.java
@@ -1,7 +1,11 @@
 package com.example.kafkaorders.config;
 
+import java.net.http.HttpClient;
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -9,8 +13,19 @@ public class HttpClientConfig {
 
     @Bean
     public RestClient restClient() {
+
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(2))
+                .build();
+
+        JdkClientHttpRequestFactory requestFactory =
+                new JdkClientHttpRequestFactory(httpClient);
+
+        requestFactory.setReadTimeout(Duration.ofSeconds(2));
+
         return RestClient.builder()
                 .baseUrl("http://localhost:8080")
+                .requestFactory(requestFactory)
                 .build();
     }
 }
